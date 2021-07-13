52fcbf6d80743ac7ce1590a1fcbde909bb924a7d
rachit2501 <rac.sri25@gmail.com>
2021-07-13 18:21:55 +0530

    fixes

--- atanSol/atan.sol
+++ atanSol/atan.sol
@@ -7,13 +7,16 @@ contract ArcTan  {
     
     using WadRayMath for uint256;
 
-    // taylor series: arctan(x) = x - x**3/x + x**5/5 - x**7/7.....
+    // taylor series: arctan(x) = x - x**3/3 + x**5/5 - x**7/7.....
+    
+    // arctan(x) + arctan(1/x)  = { pi/2 if x>0 and -pi/2 if x<0 
     
     int256 constant Wad = 1e18;
-    int256 constant TaylorSeriesVariableCount = 2;
+    int256 constant TaylorSeriesVariableCount = 3;
+    int256 pi = 3140000000000000000;
     
     // value in WAD
-    function arcTan (int256 value) public returns (int256 ){
+    function arcTan (int256 value) public pure returns (int256 ){
 
         int256 answer = 0;
         
@@ -32,8 +35,23 @@ contract ArcTan  {
         return answer;
     }
     
+    function arcTanWrapper(int256 value) public view returns (int256){
+        uint256 one = 1e18;
+        if(value < (1*Wad)){
+             int256 finalResult = arcTan(value);
+             return finalResult;
+        }
+        else {
+            value = int256(one.wadDiv(uint256(value)));
+            int256 finalResult = arcTan(value);
+            finalResult = pi/2 - finalResult;
+            return finalResult;
+        }
+        
+    }
     
-    function pow(uint256 val, uint256 p) internal returns (uint256) {
+    
+    function pow(uint256 val, uint256 p) internal pure returns (uint256) {
         
         uint256 finalVal = val;
         
@@ -43,31 +61,12 @@ contract ArcTan  {
         return finalVal;
     }
     
-    //  function u_pow(int256 x, int256 p) internal pure returns (int256) {
-    //     if (p == 0) return 1;
-    //     if (p % 2 == 1) {
-    //         return u_pow(x, p - 1).wadMul(x);
-    //     } else {
-    //         return u_pow(x, p / 2).wadMul(u_pow(x, p / 2));
-    //     }
-    // }
 
-    // function pow(int256 x, int256 p) internal pure returns (int256) {
-    //     int256 r = int256(u_pow(abs(x), p));
-    //     if (p % 2 == 1) {
-    //         return -1 * r;
-    //     }
-    //     return (r);
-    // }
-    
-    
-    // function abs(int256 x) internal pure returns (int256) {
-    //     if (x < 0) {
-    //         return int256(-x);
-    //     }
-    //     return int256(x);
-    // }
-
+    function calculateM (int256 price, int256 L )  public returns (int256 result) {
+        int256 arcTanResult = arcTanWrapper(price);
+        int256 piBy2 =  pi / 2;
+        uint256 mul = uint256(arcTanResult).wadMul(uint256(L));
+        result =  int256((mul).wadDiv(uint256(piBy2)));
+    }
 }
-
 
