65dfbacad60b952a564e43f50a827c2d0793cccf
rachit2501 <rac.sri25@gmail.com>
2023-12-18 22:57:44 +0530

    utils tests

--- sumcheck/utils_test.go
+++ sumcheck/utils_test.go
@@ -1,15 +1,17 @@
 package sumcheck
 
 import (
-	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 
 func Test_DegJ(t *testing.T) {
 	f := func(a, b, c int) int { return a*b*b*c + b + c*c*c }
-	
-	fmt.Println(DegJ(f, 0))
-	fmt.Println(DegJ(f, 1))
-	fmt.Println(DegJ(f, 2))
+
+	assert.Equal(t,DegJ(f, 0),1)
+	assert.Equal(t,DegJ(f, 1),2)
+	assert.Equal(t,DegJ(f, 2),3)
+
 }
